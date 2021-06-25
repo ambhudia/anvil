d1af641e877709b670f53764a78f504aff177beb
Ashutosh Bhudia <ashu.bhudia@gmail.com>
2021-06-25 13:15:51 -0700

    Typo

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name="forge",
     version="0.0.1",
     description="Components and utilities for Qiskit-Metal",
     long_description=readme,
     author=["Ashutosh Bhudia"],
     author_email=[
         "ashu.bhudia@gmail.com",
-        ]
+        ],
     url="https://github.com/ambhudia/forge",
     license=license,
     packages=find_packages(),
